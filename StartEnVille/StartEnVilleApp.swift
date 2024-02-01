c7ba47c934ee679613583e54d94cc4740de407ff
Charlene Hoareau <charlene.hoareau@outlook.com>
2024-02-01 19:34:40 +0100

    Initial Commit

---
+++ StartEnVille/StartEnVilleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StartEnVilleApp.swift
+//  StartEnVille
+//
+//  Created by charlene hoareau on 01/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct StartEnVilleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
